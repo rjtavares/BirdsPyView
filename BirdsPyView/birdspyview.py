35a01ee8b5e1f0a54c891f5852e7eab7f9ab473c
Ricardo Tavares <rjtavares@gmail.com>
2020-09-03 15:59:18 +0100

    Update birdspyview.py
    added team color to the player coordinates

--- BirdsPyView/birdspyview.py
+++ BirdsPyView/birdspyview.py
@@ -11,9 +11,9 @@ if image_to_open:
     image = Image.open(image_to_open)
     image = image.resize((500, int(500*image.height/image.width)))
 
-# Create a canvas component
+# Create a canvas component to draw pitch lines
 canvas_image = st_canvas(
-    fill_color = "rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    fill_color = "rgba(255, 165, 0, 0.3)", 
     stroke_width = 2,
     stroke_color = "#e00",
     background_image=image,
@@ -24,14 +24,14 @@ canvas_image = st_canvas(
 )
 
 if canvas_image.json_data["objects"]:
-    df = pd.json_normalize(canvas_image.json_data["objects"])
-    df['y1_line'] = df['top']+df['y1']
-    df['y2_line'] = df['top']+df['y2']
-    df['x1_line'] = df['left']+df['x1']
-    df['x2_line'] = df['left']+df['x2']
-    df['slope'], df['intercept'] = get_si_from_coords(df[['x1_line', 'y1_line', 'x2_line', 'y2_line']].values)
+    if len(canvas_image.json_data["objects"])>=4:
+        df = pd.json_normalize(canvas_image.json_data["objects"])
+        df['y1_line'] = df['top']+df['y1']
+        df['y2_line'] = df['top']+df['y2']
+        df['x1_line'] = df['left']+df['x1']
+        df['x2_line'] = df['left']+df['x2']
+        df['slope'], df['intercept'] = get_si_from_coords(df[['x1_line', 'y1_line', 'x2_line', 'y2_line']].values)
 
-    if len(df)>=4:
         UP_PA = line_intersect(df.loc[0, ['slope', 'intercept']], df.loc[2, ['slope', 'intercept']])
         DP_PA = line_intersect(df.loc[1, ['slope', 'intercept']], df.loc[2, ['slope', 'intercept']])
         UP_G = line_intersect(df.loc[0, ['slope', 'intercept']], df.loc[3, ['slope', 'intercept']])
@@ -40,14 +40,19 @@ if canvas_image.json_data["objects"]:
         pts_src = (UP_PA, DP_PA, UP_G, DP_G)
         pts_dst = ((82.5, 69.2), (82.5, 270.8), (0, 69.2), (0, 270.8))
         
-        st.write(pts_src)
         h,out = calculate_homography(pts_src, pts_dst)
         h_image = apply_homography_to_image(h, image)
+        team_color = st.radio("Team color: ", ['red', 'blue'])
+        if team_color == 'red':
+            stroke_color='#e00'
+        else:
+            stroke_color='#00e'
 
+        # Create a canvas component to draw pitch lines
         canvas_converted = st_canvas(
             fill_color = "rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
             stroke_width = 5,
-            stroke_color = "#e00",
+            stroke_color = stroke_color,
             background_image=Image.fromarray(h_image),
             drawing_mode= "line",
             height=340,
@@ -57,6 +62,7 @@ if canvas_image.json_data["objects"]:
 
         if canvas_converted.json_data["objects"]:
             dfCoords = pd.json_normalize(canvas_converted.json_data["objects"])
-            dfCoords['y'] = (dfCoords['top']+dfCoords['height']/2)/340*100
-            dfCoords['x'] = (dfCoords['left']+dfCoords['width']/2)/525*100
-            st.dataframe(dfCoords[['x', 'y']])
+            dfCoords['y'] = dfCoords['top']/340*100
+            dfCoords['x'] = dfCoords['left']/525*100
+            dfCoords['team'] = dfCoords.apply(lambda x: 'red' if x['stroke']=='#e00' else 'blue', axis=1)
+            st.dataframe(dfCoords[['team', 'x', 'y']])
